9bcb2061657e56bf2458c20e1410f53d397b6a35
alinalitvinka <alinalitvinko7@gmail.com>
2025-01-19 21:58:10 +0300

    feat: add 01-read-file function

---
+++ 01-read-file/index.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+
+const stream = new fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
+
+stream.on('readable', () => {
+    let data;
+    while (null !== (data = stream.read())) {
+      console.log(data);
+    }
+  });
