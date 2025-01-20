a50837e185c6ac90dfc4e4127864e930b7783312
alinalitvinka <alinalitvinko7@gmail.com>
2025-01-21 01:14:51 +0300

    feat: add 03-files-in-folder function

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+
+const commonPath = path.join(__dirname, './secret-folder');
+
+fs.readdir(commonPath, (err, files) => {
+    if (err)
+    console.log(err);
+  else {
+    files.forEach(file => {
+
+        fs.stat(commonPath.concat('\\', file), (err, stats) => {
+            const size = stats.size;
+            console.log(`${file.split('.')[0]} - ${file.split('.')[1]} - ${size}b`);
+        });
+    })
+  }
+});
