c21e2513c0c4d69d1e91aee5615ef08540f62fe7
alinalitvinka <alinalitvinko7@gmail.com>
2025-01-22 13:10:45 +0300

    fix: fix 04-copy-directory for case 'delete file' from 'files' folder

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -5,12 +5,26 @@ fs.mkdir(path.join(__dirname, '/files-copy'), { recursive: true }, (err) => {
     if (err) throw err;
 });
 
+let isEmpty;
+
+fs.readdir(path.join(__dirname, '/files-copy'), (err, files) => {
+    if (err) {
+        isEmpty = true;
+    } else {
+        files.forEach(file => {
+            fs.unlink(`${__dirname}/files-copy/${file}`, err => {
+                if(err) isEmpty = true; 
+            });
+        });
+    }
+});
+
 fs.readdir(path.join(__dirname, '/files'), (err, files) => {
     if (err) {
         throw err;
     } else {
-        files.forEach(file => {
 
+        files.forEach(file => {
             fs.copyFile(`${__dirname}/files/${file}`, `${__dirname}/files-copy/${file}`,
                 fs.constants.COPYFILE_FICLONE, (err) => {
                     if (err) {
@@ -18,5 +32,6 @@ fs.readdir(path.join(__dirname, '/files'), (err, files) => {
                     }
             });
         });
+        isEmpty = false;
     }
 });
